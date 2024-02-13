c2caf87bc657c96c15b75ab6a118538e514fe162
totty <kaqiu96@gmail.com>
2024-02-13 23:26:42 +0100

    created user entity

---
+++ src/main/java/com/amigoscode/springsecurity/entities/User.java
@@ -0,0 +1,23 @@
+package com.amigoscode.springsecurity.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
