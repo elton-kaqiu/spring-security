147f36fa5c97b385ad98db0a754614134985fd2c
totty <kaqiu96@gmail.com>
2024-02-14 07:04:42 +0100

    worked in JwtService

--- src/main/java/com/amigoscode/springsecurity/services/JwtService.java
+++ src/main/java/com/amigoscode/springsecurity/services/JwtService.java
@@ -1,10 +1,41 @@
 package com.amigoscode.springsecurity.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service 
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    @Value("${secret.key}")
+    private String secretKey;
+
     public String extractEmail(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyByte = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyByte);
     }
 }
