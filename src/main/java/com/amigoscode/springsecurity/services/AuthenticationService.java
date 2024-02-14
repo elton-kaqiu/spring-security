902db3a334fd7cd412f16c0d0bf0dcfec750fdcf
totty <kaqiu96@gmail.com>
2024-02-14 08:38:10 +0100

    we have a problem at the method extractEmail

---
+++ src/main/java/com/amigoscode/springsecurity/services/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.amigoscode.springsecurity.services;
+
+import com.amigoscode.springsecurity.entities.User;
+import com.amigoscode.springsecurity.enums.UserRole;
+import com.amigoscode.springsecurity.models.AuthenticationRequest;
+import com.amigoscode.springsecurity.models.AuthenticationResponse;
+import com.amigoscode.springsecurity.models.RegisterRequest;
+import com.amigoscode.springsecurity.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .userRole(UserRole.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findUserByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
